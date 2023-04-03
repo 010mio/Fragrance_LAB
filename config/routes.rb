3d0d8e9b027f5771543c4f257ae05a04f07e1e11
EC2 Default User <ec2-user@ip-172-31-32-98.ap-northeast-1.compute.internal>
2023-04-03 06:07:39 +0000

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #ルートパス
   root to: 'public/homes#top'
+  #アバウトパス
+  get '/about' => 'public/homes#about', as: "about"
 
 # 顧客用
 devise_for :customers,skip: [:passwords], controllers: {
@@ -12,4 +14,11 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+  end
+
+  scope module: :customer do
+    resources :articles, only: [:new, :index, :show]
+  end
 end
