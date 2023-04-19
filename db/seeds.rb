cccec1f9e5f483005f6b55fde81d067fd7b82e97
EC2 Default User <ec2-user@ip-172-31-32-98.ap-northeast-1.compute.internal>
2023-04-19 11:56:27 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#Admin.create!(
-  # email: "admin@email.com" ,
-  # password: "admin123"
-  # )
-# Customer.create!(
-  # name: "ゲスト",
-  # email: "guest@email.com" ,
-  # password: "guest123"
-  # )
-# Tag.create([
-  # { name: 'アロマ' },
-  # { name: 'シトラス' },
-  # { name: 'ハーブ' },
-  # { name: 'フローラル' },
-  # { name: 'レシピ' }
-#])
-#Article.create!(
-# customer_id: 2,                                             
-# title: 'テスト2',                                                   
-# body: 'テスト2',                                                    
-# )
-ArticleTagRelation.create!(
-  article_id: (Article.last.id - 1),
-  tag_id: (Tag.last.id - 1),
+Admin.create!(
+  email: "admin@email.com" ,
+  password: "admin123"
   )
+ Customer.create!(
+  name: "ゲスト",
+  email: "guest@email.com" ,
+  password: "guest123"
+  )
+ Tag.create([
+  { name: 'アロマ' },
+  { name: 'シトラス' },
+  { name: 'ハーブ' },
+  { name: 'フローラル' },
+  { name: 'レシピ' }
+])
