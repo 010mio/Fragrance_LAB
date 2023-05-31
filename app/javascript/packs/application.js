a5abf9a1fcfca8e583e8eef80724144e196d38b6
EC2 Default User <ec2-user@ip-172-31-32-98.ap-northeast-1.compute.internal>
2023-05-31 13:20:55 +0000

    タグ検索修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,11 @@ import './article';
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('DOMContentLoaded', function() {
+    let tag_name = document.querySelector("#tag_name")
+    tag_name.addEventListener('change', function() {
+        console.log(tag_name.value)
+        tag_name.parentNode.submit();
+    })
+})
