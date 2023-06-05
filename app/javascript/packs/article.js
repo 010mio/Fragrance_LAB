971ca5b17b04b156a18873605930df2b22720f4f
EC2 Default User <ec2-user@ip-172-31-32-98.ap-northeast-1.compute.internal>
2023-06-05 10:06:27 +0000

    修正

--- app/javascript/packs/article.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let tag_name = document.querySelector("#tag_name")
-    tag_name.addEventListener('change', function() {
-        console.log(tag_name.value)
-        tag_name.parentNode.submit();
-    })
-})
